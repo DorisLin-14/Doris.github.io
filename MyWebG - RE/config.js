c6d915bda48aa1d2b84607ebef8ec1af9b6be0e2
DorisLin-14 <lindorisbaby@gmail.com>
2026-02-11 15:16:28 +0800

    Delete MyWebG - RE directory

--- MyWebG - RE/config.js
+++
@@ -1,9 +0,0 @@
-// 整個遊戲的設定
-const config = {
-    storyFolder: './story/',
-    bgFolder: './assets/bg/',
-    charaFolder: './assets/chara/',
-    bgmFolder: './assets/bgm/',
-    seFolder: './assets/se/',
-    initialChapter: 'VL_Chapter1.txt',
-};
