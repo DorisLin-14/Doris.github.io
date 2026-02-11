1ecf70aef17ee900a0230b0d7d0f929d3c15c35e
DorisLin-14 <lindorisbaby@gmail.com>
2026-02-11 16:15:04 +0800

    Add files via upload

---
+++ MyWebG - RE/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    storyPath: './story/',
+    assets: {
+        bg: './assets/bg/',
+        chara: './assets/chara/',
+        bgm: './assets/bgm/',
+        se: './assets/se/'
+    }
+};
