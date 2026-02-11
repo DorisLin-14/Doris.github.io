352d0c7e9bb390bacfdf1edd7fbb1f7bfdf1faab
DorisLin-14 <lindorisbaby@gmail.com>
2026-02-11 12:06:15 +0800

    Add files via upload

---
+++ MyWebG - RE/config.js
@@ -0,0 +1,9 @@
+// 整個遊戲的設定
+const config = {
+    storyFolder: 'story/',
+    bgFolder: 'assets/bg/',
+    charaFolder: 'assets/chara/',
+    bgmFolder: 'assets/bgm/',
+    seFolder: 'assets/se/',
+    initialChapter: 'VL_Chapter1.txt',
+};
