0abc69c36ff51997d93286d0a080d461aaa52d88
DorisLin-14 <lindorisbaby@gmail.com>
2026-02-11 19:37:55 +0800

    Delete MyWebG - RE directory

--- MyWebG - RE/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    storyPath: './story/',
-    assets: {
-        bg: './assets/bg/',
-        chara: './assets/chara/',
-        bgm: './assets/bgm/',
-        se: './assets/se/'
-    }
-};
