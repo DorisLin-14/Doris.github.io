43090afb972e0500b4fd125a55cbf9a7c41be033
DorisLin-14 <lindorisbaby@gmail.com>
2026-02-12 08:38:11 +0800

    Add files via upload

---
+++ MyWebG - RE/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    storyPath: './story/',
+    assets: {
+        bg: './assets/bg/',
+        chara: './assets/chara/',
+        bgm: './assets/bgm/',
+        se: './assets/se/'
+    },
+
+    // 打字設定
+    textSpeed: 200,        // 0.2秒一字
+    afterLineDelay: 1000,  // 一句完整後1秒才能點擊
+
+    // 畫面效果
+    bgFadeTime: 800,
+    charaFadeTime: 500
+};
